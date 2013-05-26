49545166599d96506a8a7e42bc02a9c76b37e89a
kcy1860 <ssdut.1860@gmail.com>
2013-05-26 16:02:03 -0400

    计步部分

--- CaloriesCounter/src/com/caloriecounter/portal/HomeActivity.java
+++ CaloriesCounter/src/com/caloriecounter/portal/HomeActivity.java
@@ -1,78 +1,187 @@
 package com.caloriecounter.portal;
 
-import com.example.caloriescounter.R;
+import java.util.GregorianCalendar;
 
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.caloriescounter.R;
 
 public class HomeActivity extends Activity {
+
+	public static final String S_TAG = "G-SENSOR";
+	public static final String S_NOTIFICATION_TITLE = "G-SENSOR";
+	public static final String S_NOTIFICATION_CONTENT = "recording your steps now";
+
+	private TextView mTextview1;
+	private TextView mTextview2;
 	private Button mButtonStart;
 	private Button mButtonEnd;
+	private ImageView mImageView;
+
 	private Context mContext;
+
+	private SensorManager sensorManager;
+	private SensorEventListener sel;
+	private NotificationManager mNotificationManager;
+	Sensor accelerometerSensor;
+
+	int duration = 0;
+	// private int durationPause = 0;
+	// private int dura = 0;
+	float[] s;
+	float[] s1;
+
+	GregorianCalendar timeStarted;
+	int step = 0;
+	int flag = 0;
+	int i1 = 0;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
-		System.out.println("tab1");
 
 		mContext = this;
 
+		mImageView = (ImageView) findViewById(R.id.imageProfile);
+		mImageView.setImageResource(R.drawable.icon);
+
+		mTextview1 = (TextView) findViewById(R.id.s_text1);
+		mTextview2 = (TextView) findViewById(R.id.s_text2);
 		mButtonStart = (Button) findViewById(R.id.btnStart);
-
-		mButtonStart.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				// new
-				// AlertDialog.Builder(mContext).setTitle("ActivityType").setItems(
-				// new String[] { "Auto", "Walking" }, null).setNegativeButton(
-				// "Start", null).show();
-				new AlertDialog.Builder(mContext)
-						.setTitle("ActivityType")
-						.setMultiChoiceItems(
-								new String[] { "Auto", "Walking", "Running" },
-								null, null).setPositiveButton("Confirm", null)
-						.setNegativeButton("Cancel", null).show();
-				AlertDialog.Builder builder = new Builder(mContext);
-				builder.setTitle("ActivityType");
-				builder.setMultiChoiceItems(new String[] { "Auto", "Walking",
-						"Running", "Jumping", "Standing" }, null, null);
-				builder.setPositiveButton("Confirm",
-						new DialogInterface.OnClickListener() {
-
-							@Override
-							public void onClick(DialogInterface dialog,
-									int which) {
-								// TODO Auto-generated method stub
-
-							}
-						});
-
-				builder.setNegativeButton("Cancel",
-						new DialogInterface.OnClickListener() {
-
-							@Override
-							public void onClick(DialogInterface dialog,
-									int which) {
-								// TODO Auto-generated method stub
-
-							}
-						});
-
-			}
-		});
-
 		mButtonEnd = (Button) findViewById(R.id.btnEnd);
+
+		mTextview1.setText(String.valueOf(step));
+		mTextview2.setText(String.valueOf(duration));
+
+		s = new float[200];
+		s1 = new float[200];
+		for (int i = 0; i < 200; i++) {
+			s[i] = 0;
+			s1[i] = 0;
+		}
+
+		sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+		if (sensorManager.getSensorList(Sensor.TYPE_ACCELEROMETER).size() == 0) {
+			mTextview1.setText("wrong!");
+		}
+
+		if (savedInstanceState != null) {
+			step = savedInstanceState.getInt("STEPSAVED");
+			duration += savedInstanceState.getInt("TIMESAVED");
+			// durationPause = savedInstanceState.getInt("TIMESAVED");
+
+		}
+
+		sel = new SensorEventListener() {
+
+			@Override
+			public void onAccuracyChanged(Sensor arg0, int arg1) {
+				// TODO Auto-generated method stub
+			}
+
+			@SuppressWarnings("deprecation")
+			@Override
+			public void onSensorChanged(SensorEvent event) {
+				// TODO Auto-generated method stub
+				float x = event.values[SensorManager.DATA_X];
+				float y = event.values[SensorManager.DATA_Y];
+				float z = event.values[SensorManager.DATA_Z];
+
+				for (int i = 1; i < 200; i++) {
+					s1[i - 1] = s[i];
+				}
+				s1[199] = y;
+
+				if (s1[0] != 0) {
+					flag = 1;
+					mTextview1.setText("s1[0]!=0");
+				}
+				if (flag == 1) {
+					for (int i = 1; i < 30; i++) {
+						if (s1[100] > s1[100 - i]) {
+							if (s1[100] > 13)
+								i1++;
+						}
+					}
+					for (int i = 1; i < 30; i++) {
+						if (s1[100] > s1[100 + i]) {
+							if (s1[100] > 13)
+								i1++;
+						}
+					}
+
+					if (i1 == 58)
+						step++;
+				}
+
+				mTextview1.setText(String.valueOf(step));
+				duration = ((int) ((System.currentTimeMillis() - timeStarted
+						.getTimeInMillis()) / 1000));
+				mTextview2.setText(String.valueOf(duration));
+				// dura = duration;
+
+				i1 = 0;
+				flag = 0;
+				for (int i = 0; i < 200; i++) {
+					s[i] = s1[i];
+				}
+			}
+		};
+
+		accelerometerSensor = sensorManager
+				.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+		OnClickListener mButtonStartListener = new Button.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				// accelerometerSensor =
+				// sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+				sensorManager.registerListener(sel, accelerometerSensor,
+						SensorManager.SENSOR_DELAY_FASTEST);
+				timeStarted = new GregorianCalendar();
+			}
+		};
+
+		mButtonStart.setOnClickListener(mButtonStartListener);
+
 		mButtonEnd.setOnClickListener(new View.OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
+
+				// ---------------------
+				if (flag == 0) {
+					sensorManager.unregisterListener(sel);
+					mTextview1.setText(String.valueOf(step));
+					duration = ((int) ((System.currentTimeMillis() - timeStarted
+							.getTimeInMillis()) / 1000));
+					// dura = duration;
+					mTextview2.setText(String.valueOf(duration));
+				}
+
 				AlertDialog.Builder builder = new Builder(mContext);
 				builder.setTitle("Your Choice");
 				builder.setMessage("Save to diary?");
@@ -99,11 +208,93 @@ public class HomeActivity extends Activity {
 			}
 		});
 
+		Intent i = new Intent(this.getApplication(), MainActivity.class);
+		i.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+		PendingIntent pi = PendingIntent.getActivity(this, 0, i, 0);
+
+		Notification notification = new Notification.Builder(this)
+				.setContentTitle(S_NOTIFICATION_TITLE)
+				.setContentText(S_NOTIFICATION_CONTENT)
+				.setSmallIcon(R.drawable.icon).setContentIntent(pi).build();
+		mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+		notification.flags = notification.flags
+				| Notification.FLAG_ONGOING_EVENT;
+
+		mNotificationManager.notify(0, notification);
+
 	}
 
 	@Override
-	protected void onRestart() {
-		super.onRestart();
-		System.out.println("tab11");
+	/*
+	 * protected void onRestart() { super.onRestart();
+	 * System.out.println("tab11"); }
+	 */
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.home, menu);
+		return true;
 	}
+
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		// Save the tab index before the activity goes into background.
+		// Referred by string key TAB_INDEX_KEY
+		int stepSaved = Integer.valueOf(mTextview1.getText().toString());
+		outState.putInt("STEPSAVED", stepSaved);
+		int timeSaved = Integer.valueOf(mTextview2.getText().toString());
+		outState.putInt("TIMESAVED", timeSaved);
+	}
+
+	protected void onResume() {
+		super.onResume();
+		// Sensor accelerometerSensor =
+		// sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		// sensorManager.registerListener(sel,
+		// accelerometerSensor,SensorManager.SENSOR_DELAY_FASTEST);
+		// timeStarted = new GregorianCalendar();
+
+	}
+
+	protected void onPause() {
+		// sensorManager.unregisterListener(sel);
+		// mytextview1.setText("step:"+step);
+		// mTextview1.setText(String.valueOf(step));
+		// durationPause += ((int) ((System.currentTimeMillis() - timeStarted
+		// .getTimeInMillis()) / 1000));
+
+		// --------------------
+		// dura = durationPause;
+		// duration = ((int) ((System.currentTimeMillis() - timeStarted
+		// .getTimeInMillis()) / 1000));
+		// dura = duration;
+		// mTextview2.setText(String.valueOf(duration));
+
+		// mTextview2.setText(String.valueOf(durationPause));
+		super.onPause();
+	}
+
+	public void onDestroy() {
+		((NotificationManager) getSystemService(NOTIFICATION_SERVICE))
+				.cancelAll();
+		// finish();
+		super.onDestroy();
+	}
+
+	public void onBackPressed() {
+
+		((NotificationManager) getSystemService(NOTIFICATION_SERVICE))
+				.cancelAll();
+		super.onBackPressed();
+	}
+
+	public int getStep() {
+		return step;
+	}
+
+	public int getDuration() {
+		// dura = duration;
+		// mTextview2.setText(String.valueOf(duration));
+		return duration;
+	}
+
 }
