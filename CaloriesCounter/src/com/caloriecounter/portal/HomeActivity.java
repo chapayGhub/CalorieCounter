563442775c744df52f74c963d62f7ee5d34946a5
kcy1860 <ssdut.1860@gmail.com>
2013-05-27 15:15:38 -0400

    计步

--- CaloriesCounter/src/com/caloriecounter/portal/HomeActivity.java
+++ CaloriesCounter/src/com/caloriecounter/portal/HomeActivity.java
@@ -262,7 +262,7 @@ public class HomeActivity extends Activity {
 									mDataSourceBridge.insertCalorie(c);
 
 									mDataSourceBridge.close();
-									// jumpToShow();
+									 jumpToShow();
 
 								}
 							});
@@ -271,7 +271,7 @@ public class HomeActivity extends Activity {
 								@Override
 								public void onClick(DialogInterface dialog,
 										int which) {
-									// jumpToShow();
+									jumpToShow();
 								}
 							});
 					step = 0;
